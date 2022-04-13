11d81cc504b784652d33907826aec08524681288
junyicc <cai_junyi@yeah.net>
2022-04-13 10:46:01 +0800

    :art: get trace id from header

--- middleware/logger.go
+++ middleware/logger.go
@@ -8,10 +8,19 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	X_Trace_Id = "X-Trace-Id"
+)
+
 // WrapLogger wraps logger with trace id
 func WrapLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		zl := logging.With(zap.String("TraceId", genTraceId()))
+		// get trace id from header
+		traceID := c.GetHeader(X_Trace_Id)
+		if traceID == "" {
+			traceID = genTraceId()
+		}
+		zl := logging.With(zap.String("TraceId", traceID))
 		// AddCallerSkip(-1) 保证调用者的信息能正确输出
 		zl.SetLogger(zl.GetLogger().WithOptions(zap.AddCallerSkip(-1)))
 
